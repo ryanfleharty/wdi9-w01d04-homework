566a0ceef3b4efea78ab215dcc1ebcebf7b98708
MarchWaden <max.linz@live.com>
2018-09-13 23:21:45 -0600

    Answered part one

---
+++ javascript/app.js
@@ -0,0 +1,10 @@
+console.log("javascript is working");
+
+//1. A parameter is a variable declared as one of the inputs of the function, 
+//an argument is the actual value the function recieves.
+
+//2. A return will return the value specified to whatever called the function,
+//and terminate the function. A console log just logs something to the console and nothing else.
+
+//3. You can call a function where a value should go and the expression will evaluate
+//with the value the function returns in that place. This can even be inside a call to another function.
