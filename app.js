376b16d76f3e0057e3475bcbe743986f1fc1c1fc
Jack Bogart <bogart.jack98@gmail.com>
2018-09-14 08:48:09 -0600

    Commit 1 - Verbal Questions

---
+++ app.js
@@ -0,0 +1,8 @@
+//Q1.1. A paramater is the variable that is declared in a function definition/expression. Arguments are passed when the function is called
+//
+//Q1.2. return is the value that the function will be evaluated to when it is invoked. A console.log() call simply prints out a value to the console/terminal
+//
+//Q1.3. When a function is called it will be evaluated and a value will be returned, so when you call the function it will evaluate to some value 
+//
+
+//Q2. Palindrome
