45f2206f7afe73ddb123e9593428037b5adaaff1
MarchWaden <max.linz@live.com>
2018-09-13 23:27:53 -0600

    Finished palindrome function

--- javascript/app.js
+++ javascript/app.js
@@ -8,3 +8,14 @@ console.log("javascript is working");
 
 //3. You can call a function where a value should go and the expression will evaluate
 //with the value the function returns in that place. This can even be inside a call to another function.
+const check_palindrome = (word) => {
+	if (word === word.split("").reverse().join("")) { 
+		return true;
+	}
+	return false
+}
+//So there's no reverse string, but you can split it into 
+//an array of its constituent characters and reverse that, then join them together.
+//Since that's what I was about to do manually I don't feel too bad about looking up the
+//methods to do that in one line, even if it *looks* like I'm using magic (to me) code.
+console.log(check_palindrome("ada"))
